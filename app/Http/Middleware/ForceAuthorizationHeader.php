6c2ad4ee03d7d8f998001d403aeb47989341b441
kamleshsukhwal <kamlesh@htcl.co.in>
2026-01-02 10:47:44 +0530

    Fix Authorization header handling for cPanel/Apache

--- app/Http/Middleware/ForceAuthorizationHeader.php
+++ app/Http/Middleware/ForceAuthorizationHeader.php
@@ -9,8 +9,20 @@ class ForceAuthorizationHeader
 {
     public function handle(Request $request, Closure $next)
     {
-        if (! $request->header('Authorization') && isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            $request->headers->set('Authorization', $_SERVER['HTTP_AUTHORIZATION']);
+        if (! $request->header('Authorization')) {
+
+            if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+                $request->headers->set('Authorization', $_SERVER['HTTP_AUTHORIZATION']);
+            }
+            elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $request->headers->set('Authorization', $_SERVER['REDIRECT_HTTP_AUTHORIZATION']);
+            }
+            elseif (function_exists('apache_request_headers')) {
+                $headers = apache_request_headers();
+                if (isset($headers['Authorization'])) {
+                    $request->headers->set('Authorization', $headers['Authorization']);
+                }
+            }
         }
 
         return $next($request);
