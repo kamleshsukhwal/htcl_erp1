285b44fe5c8d09fc01d6e76476c4aa550664b464
kamleshsukhwal <kamlesh@htcl.co.in>
2026-01-02 10:26:15 +0530

    added logout middleware
    for token store

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
+use App\Http\Middleware\ForceAuthorizationHeader;
 use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )->withMiddleware(function (Middleware $middleware): void {
+          $middleware->  $middleware->append(ForceAuthorizationHeader::class);
           $middleware->alias([
         'role' => RoleMiddleware::class,
         'permission' => PermissionMiddleware::class,
