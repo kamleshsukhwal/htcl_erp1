78af7fecb0e8da8772ece1b8fa0390e1b3e3ddd2
kamleshsukhwal <kamlesh@htcl.co.in>
2026-02-25 18:50:32 +0530

    QA, NRC module
    assed QA, NCR api

--- app/Http/Controllers/Api/Quality/NcrController.php
+++ app/Http/Controllers/Api/Quality/NcrController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Quality;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ncr;
+use App\Models\QaAttachment;
 use Illuminate\Http\Request;
 
 class NcrController extends Controller
@@ -17,4 +18,20 @@ class NcrController extends Controller
         'data' => $ncr
     ]);
 }
+
+
+public function upload(Request $request,$id)
+{
+    $file = $request->file('file');
+    $path = $file->store('qa','public');
+
+    QaAttachment::create([
+        'module' => 'ncr',
+        'module_id' => $id,
+        'file_path' => $path
+    ]);
+
+    return response()->json(['message'=>'Uploaded']);
+}
+
 }
