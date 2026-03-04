9fc7058ba548e5b0b43249e0f346817bf1d732ce
kamleshsukhwal <kamlesh@htcl.co.in>
2026-03-04 10:24:46 +0530

    multiple inspection

--- app/Http/Controllers/Api/Quality/AuditLogController.php
+++ app/Http/Controllers/Api/Quality/AuditLogController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api\Quality;
 
 use App\Http\Controllers\Controller;
+use App\Models\AuditLog;
 use Illuminate\Http\Request;
 
 class AuditLogController extends Controller
 {
-    
+    public function byModule($module)
+{
+    $logs = AuditLog::where('module_name', $module)->latest()->get();
+
+    return response()->json([
+        'data' => $logs
+    ]);
+}
+
+public function byProject($projectId)
+{
+    $logs = AuditLog::where('project_id', $projectId)->latest()->get();
+
+    return response()->json([
+        'data' => $logs
+    ]);
+}
+
+public function byUser($userId)
+{
+    $logs = AuditLog::where('user_id', $userId)->latest()->get();
+
+    return response()->json([
+        'data' => $logs
+    ]);
+}
 }
